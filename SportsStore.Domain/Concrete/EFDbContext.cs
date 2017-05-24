a9791a6fa19c8d2c8ba664fc772b32b97a94a651
colin.phelps <colin.phelps@zirmed.com>
2017-05-24 17:01:21 -0400

    working on unit tests

--- SportsStore.Domain/Concrete/EFDbContext.cs
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using SportsStore.Domain.Entities;
 
-namespace SportsStore.Domain.Entities
+namespace SportsStore.Domain.Concrete
 {
     public class EFDbContext : DbContext
     {
-        public DbSet<Product> Products { get; set; }
+       public DbSet<Product> Products { get; set; }
     }
 }
---
+++ SportsStore.UnitTests/CartTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System.Linq;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Controllers;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples" },
+            }.AsQueryable());
+
+            // Arrange - create a cart
+            Cart cart = new Cart();
+
+            // Arrange - create the controller
+            CartController target = new CartController(mock.Object);
+
+            // Act - add a product to the cart
+            target.AddToCart(cart, 1, null);
+
+        }
+    }
+}
